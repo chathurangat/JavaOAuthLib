19907a4d6eeda099a21e8659e811c61986eee1ed
darshanac <darshanac@hsenidmobile.com>
2013-02-06 10:22:33 +0530

    OAuthException class is added qith new exception package

---
+++ src/main/java/org/fosshub/oauth/exception/OAuthException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2013.
+ * @Author Chathuranga Tennakoon
+ * @Blog chathurangat.blogspot.com
+ * @Web  www.fosshub.org
+ * @Email chathuranga.t@gmail.com
+ */
+package org.fosshub.oauth.exception;
+//todo exception handling
+public class OAuthException extends RuntimeException{
+
+//    private int errorCode;
+    private String exceptionMessage;
+
+    public OAuthException(String message, Throwable throwable) {
+        super(throwable);
+        this.exceptionMessage = message;
+    }
+
+    public OAuthException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public OAuthException(String message) {
+        super(message);
+        this.exceptionMessage = message;
+    }
+
+    public OAuthException() {
+        super();
+    }
+
+    public String getExceptionMessage() {
+        return exceptionMessage;
+    }
+
+    public void setExceptionMessage(String exceptionMessage) {
+        this.exceptionMessage = exceptionMessage;
+    }
+}
